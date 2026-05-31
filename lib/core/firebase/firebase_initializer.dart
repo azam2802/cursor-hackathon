6377a2817aeb1d90ad5ba20d93a094808b9af179
Azam <azamumarov182@gmail.com>
2026-05-31 17:25:49 +0600

    init: firebase

---
+++ lib/core/firebase/firebase_initializer.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+import '../../firebase_options.dart';
+
+Future<void> initializeFirebase() async {
+  if (Firebase.apps.isNotEmpty) {
+    return;
+  }
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
